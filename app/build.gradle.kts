bc813bb88de0b08afb7e8c554bc02e42e60582ce
hzdawoud <hzdawoud@gmail.com>
2025-03-06 20:02:53 +0200

    initial project setup with required libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.hilt)
+    kotlin("kapt")
 }
 
 android {
@@ -56,4 +58,15 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // retrofit
+    implementation(libs.retrofit)
+    implementation(libs.gson.converter)
+
+    // coroutines
+    implementation(libs.coroutines)
+
+    // hilt dependency injection
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
 }
---
+++ app/src/main/java/com/hzdawoud/fetchlt/FetchltApplication.kt
@@ -0,0 +1,8 @@
+package com.hzdawoud.fetchlt
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FetchltApplication : Application() {
+}
